28e6e95d6a0ec1b615d67a2fe33e986373e2e8ff
Angel <angelSistemas21@gmail.com>
2022-11-30 01:58:51 -0600

    add: Component initial structure

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+export const GifExpertApp = () =>{
+    return (
+        <>
+            {/* Title */}
+            <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Listado de Items */}
+
+                {/* Items */}
+        </>
+    )
+}

5d4151fde29d08f566a2993454c638b06d2c3dca
Angel <angelSistemas21@gmail.com>
2022-12-03 16:37:05 -0600

    feat: Function onAddCategory

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,14 +1,38 @@
+import { useState } from "react"
+import { AddCategory, GifGrid } from "./components";
+
 export const GifExpertApp = () =>{
+
+    const [categories, setCategories] = useState(['One Punch']);
+
+    const onAddCategory = (newCategory) => {
+        
+        if(categories.includes(newCategory)) return;
+
+        setCategories([newCategory, ...categories])
+    }
+
     return (
         <>
             {/* Title */}
             <h1>GifExpertApp</h1>
 
             {/* Input */}
+            <AddCategory 
+                onNewCategory={ onAddCategory }
+            />
 
             {/* Listado de Items */}
-
-                {/* Items */}
+            {
+                categories.map( (category) =>
+                // Va a regresar un objeto por eso se ponen parentesis
+                (
+                    <GifGrid 
+                        key={ category } 
+                        category={ category }
+                    />        
+                ))
+            }
         </>
     )
 }
